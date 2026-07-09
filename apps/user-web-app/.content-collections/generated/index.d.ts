6f80db24aa92e084812d7f1f4e19ad4c86e2e1fb
Alexander Asomba <3323537+alexasomba@users.noreply.github.com>
2026-07-09 16:57:27 +0100

    chore: ignore .content-collections in git

--- apps/user-web-app/.content-collections/generated/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { GetTypeByName } from "@content-collections/core";
-
-import type configuration from "../../content-collections.ts";
-
-export type Speaker = GetTypeByName<typeof configuration, "speakers">;
-export declare const allSpeakers: Array<Speaker>;
-
-export type Talk = GetTypeByName<typeof configuration, "talks">;
-export declare const allTalks: Array<Talk>;
